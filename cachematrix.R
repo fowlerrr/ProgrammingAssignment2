a3158d8fca5a965a7f4418dc7b319596408f7eca
Ryan Fowler <fowlerrr@gmail.com>
2015-06-16 22:36:56 +0100

    Initial Checkin
    Tested against matrix(c(-1, -2, 1, 1), 2,2)
    (https://class.coursera.org/rprog-015/forum/thread?thread_id=447)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##
+## makeCacheMatrix -
+## This function creates a special "matrix" object that can cache its inverse.
+##
+## cacheSolve
+## Return a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
 
+## creates a cacheable matrix, ie. one that
+## will cache it's inverse and return that if
+## possible.  This will hold true as long as
+## set() is not called.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<- NULL
+  set <- function(y) {
+    x<<- y
+    inv<<- NULL
+  }
+  get <- function() x 
+  setinv<- function(i) inv<<- i
+  getinv<- function() inv
+  list(set = set, get = get,
+       setinv= setinv,
+       getinv= getinv)
 }
 
 
-## Write a short comment describing this function
-
+## given a cacheMatrix, solves for the inverse
+## matrix.  Will check the cached result first.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!is.null(x$getinv()))
+  {
+    message("using cached result")
+    return(x$getinv())
+  }
+  
+  x$setinv(solve(x$get()))
+  x$getinv()
 }
